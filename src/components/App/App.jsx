7009ffe41136e3a4ee92c38d026e9ac719acabe0
Rafael Silva <rafsilva004@gmail.com>
2023-09-24 20:51:37 -0500

    changed router code to fix issue

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
-//components
+// Import your components
 import Feeling from '../Feedback/Feeling';
 import Header from '../Header/Header';
 import Understanding from '../Feedback/Understanding';
@@ -11,40 +11,17 @@ import Comments from '../Feedback/Comments';
 import Review from '../Review/Review';
 
 function App() {
-
   return (
     <div className='App'>
-
       <Router>
-        <Route exact path="/" />
-        <Header />
+        {/* Define your routes within this Router component */}
+        <Route exact path="/" component={Header} />
+        <Route exact path="/feeling" component={Feeling} />
+        <Route exact path="/understanding" component={Understanding} />
+        <Route exact path="/support" component={Support} />
+        <Route exact path="/comments" component={Comments} />
+        <Route exact path="/review" component={Review} />
       </Router>
-
-      <Router>
-        <Route exact path="/feeling" />
-        <Feeling />
-      </Router>
-
-      <Router>
-        <Route exact path="/understanding" />
-        <Understanding />
-      </Router>
-
-
-      <Router>
-        <Route exact path="/support" />
-        <Support />
-      </Router>
-
-      <Router>
-        <Route exact path="/comments" />
-        <Comments />
-      </Router>
-
-      <Router>
-        <Review exact path="/review" />
-      </Router>
-
     </div>
   );
 }
